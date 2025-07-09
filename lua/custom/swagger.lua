88e4fa9d544ecfc2101b618ed7f48fc6012d4534
Gustav <gurraarre5@gmail.com>
2025-07-09 19:21:12 +0200

    swagger_comment

--- lua/custom/swagger.lua
+++ lua/custom/swagger.lua
@@ -2,15 +2,15 @@ local M = {}
 
 function M.insert_swagger_doc()
     local swagger_lines = {
-        "--- @Summary Summary of the endpoint",
-        "--- @Description Description of what this endpoint does",
-        "--- @Tags tag1, tag2",
-        "--- @Accept json",
-        "--- @Produce json",
-        "--- @Param id path int true \"ID of the object\"",
-        "--- @Success 200 {object} model.Object",
-        "--- @Failure 400 {object} model.ErrorResponse",
-        "--- @Router /path [get]",
+        "// @Summary Summary of the endpoint",
+        "// @Description Description of what this endpoint does",
+        "// @Tags tag1, tag2",
+        "// @Accept json",
+        "// @Produce json",
+        "// @Param id path int true \"ID of the object\"",
+        "// @Success 200 {object} model.Object",
+        "// @Failure 400 {object} model.ErrorResponse",
+        "// @Router /path [get]",
     }
 
     local cursor = vim.api.nvim_win_get_cursor(0)
